dc94a265fdb9fa7f073f94f244e8fd5ecf85d61b
srFelipes <37315690+srFelipes@users.noreply.github.com>
2023-04-19 10:53:10 -0400

    refactoring

--- src/serial_wioe5.py
+++ src/serial_wioe5.py
@@ -2,11 +2,14 @@
 serial_wioe5.py is a python module used to communicate via serial with the wio e5,
 based on the information at https://files.seeedstudio.com/products/317990687/res/LoRa-E5%20AT%20Command%20Specification_V1.0%20.pdf
 """
+import logging
 import serial
 import serial.tools.list_ports
 from src.wio_errors import *
 baudrate = 9600
 timeout = 1
+
+logger = logging.getLogger(__name__)
 
 def ports():
     """
@@ -24,20 +27,42 @@ class Wioe5:
         initialization method, a port can be given to it, otherwise it will try
         to open /dev/ttyUSB1
         """
-        if not serial_port:
-            serial_port = serial.Serial('/dev/ttyUSB0', baudrate)
-        self.connection = serial.Serial(port =serial_port.port,timeout=timeout)
+        if serial_port:
+            if serial.Serial==type(serial_port):
+                self.connection = serial.Serial(port=serial_port.port,
+                                                timeout=timeout)
+            elif str==type(serial_port):
+                self.connection = serial.Serial(port=serial_port,
+                                                baudrate=baudrate,
+                                                timeout=timeout)
+            
+        else:
+            self.connection = serial.Serial('/dev/ttyUSB0', baudrate)
+        
         self.write('AT')
         self.mode = None
         self.error = None
+
     def write(self,command : str):
         """
         Write method, it checks if there is an error in the answer before returning it
         """
+        logger.debug('command %s',command)
         if not b'\n' == command[-1]:
             command += '\n'
         self.connection.write(command.encode('UTF-8'))
         self.connection.flush()
         answer = self.connection.readline()
-        wioError(answer)
+        try:
+            wioError(answer)
+        except Exception as e:
+            logger.error('answer \"%s\" is an error',answer,exc_info=e)
+            raise e
+        logger.debug('answer was %s',answer)
         return answer
+    
+    def get_mode(self):
+        """
+        Ask the mode to the radio
+        """
+        query = 'AT+MODE'
--- test/test_serial_wioe5.py
+++ test/test_serial_wioe5.py
@@ -14,19 +14,19 @@ def serial_port()->mock_serial.MockSerial:
 
 @pytest.fixture
 def wioe5(serial_port)->Wioe5:
-    Wioe5(serial_port)
+    Wioe5(serial_port.port)
 
 def test_at_called(serial_port):
-    wioe = Wioe5(serial_port)
+    wioe = Wioe5(serial_port.port)
     assert serial_port.stubs['AT'].called
 
 def test_at_not_ok(serial_port: mock_serial.MockSerial,wioe5):
     serial_port.stub(name = 'AT',receive_bytes = b'AT\n',
                      send_bytes=b'+AT: ERROR(-11)\n')
     try:
-        wio = Wioe5(serial_port=serial_port)
+        wio = Wioe5(serial_port=serial_port.port)
         assert False
     except Wioe5WrongFormatError:
         assert True
     assert serial_port.stubs['AT'].called
-    
+
