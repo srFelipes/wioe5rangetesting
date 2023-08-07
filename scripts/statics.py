babf6f4fb0309f1db7a6fabc272a551f4c3f7aaa
srFelipes <37315690+srFelipes@users.noreply.github.com>
2023-08-07 12:16:49 -0400

    moved utils to a new utils file

--- scripts/statics.py
+++ scripts/statics.py
@@ -1,42 +1,7 @@
 import argparse
 import json
 import pickle as pkl
-
-def raw_msg_to_int(input : str):
-    return_int = 0
-    for i in range(int(len(input)/2)):
-        sub_data = input[i*2:i*2+2]
-        try:
-            return_int += int(sub_data,base=16)<<i*8
-        except:
-            return -1
-    return return_int
-
-
-def get_last_data(file):
-    """
-    return a list of the last data in the file
-    """
-    output = []
-    while True:
-        line = file.readline()
-        if 'entering continuous RX test mode' in line:
-            
-            output  = []
-        elif 'received the following packet' in line:
-            time_of_data = line[:line.find(' [')]
-            data_raw = line[line.find('{'):-1]
-            data_raw = data_raw.replace('\'','\"')
-            data = json.loads(data_raw)
-            process_dict = {'time':time_of_data,
-                            'id':data['MSG'][:2],
-                            'num':raw_msg_to_int(data['MSG'][2:])}
-            data.update(process_dict)
-            output.append(data)
-
-        elif '' == line:
-            return output
-        
+from src.utils.utils import *
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Script to calculate statics in the data that is in logs",
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
